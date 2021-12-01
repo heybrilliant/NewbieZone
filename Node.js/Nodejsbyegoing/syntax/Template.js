a071f88daf9a23ee4ff0f226b9eab0ae12bb9055
EunKyung Shin <heybrilliante@gmail.com>
2021-12-01 15:13:48 +0900

    Node.js by egoin(JavaScript review)

---
+++ Node.js/Nodejsbyegoing/syntax/Template.js
@@ -0,0 +1,8 @@
+var name = 'ekshin';
+// ` : Template의 시작과 끝을 나타내는 기호
+
+var letter = `Dear ${name}
+
+Lorem ipsum 어쩌고 저쩌고 ${name}`
+
+console.log(letter);
---
+++ Node.js/Nodejsbyegoing/syntax/Variable.js
@@ -0,0 +1,8 @@
+
+// 대입연산자
+a = 1;
+console.log(a);
+a = 2;
+console.log(b);
+
+var a = 1;

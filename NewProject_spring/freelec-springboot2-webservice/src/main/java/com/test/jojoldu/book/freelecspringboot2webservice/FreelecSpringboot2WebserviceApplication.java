a9dfcc8ee062efa7fe72f2b9d91f52177abbf067
EunKyung Shin <heybrilliante@gmail.com>
2021-12-09 17:31:24 +0900

    211209 create Springboot basic(test) codes

--- NewProject_spring/freelec-springboot2-webservice/src/main/java/com/test/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
+++ NewProject_spring/freelec-springboot2-webservice/src/main/java/com/test/jojoldu/book/freelecspringboot2webservice/FreelecSpringboot2WebserviceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FreelecSpringboot2WebserviceApplication {
-
+public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(FreelecSpringboot2WebserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
+
 	}
 
 }

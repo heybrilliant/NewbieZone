eaa96405ddcc4bbcf587b0917211cc774597fdbe
EunKyung Shin <heybrilliante@gmail.com>
2021-12-14 13:11:06 +0900

    211214 update project

--- NewbieProject_SpringBoot/newbie_project/src/test/java/com/newbie/springboot/web/HelloControllerTest.java
+++ NewbieProject_SpringBoot/newbie_project/src/test/java/com/newbie/springboot/web/HelloControllerTest.java
@@ -8,9 +8,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -26,6 +28,19 @@ public class HelloControllerTest {
         mvc.perform(get("/hello")) // MockMvc를 통해 /hello 주소로 HTTP GET 요청
                 .andExpect(status().isOk()) // mvc.perform의 결과를 검증 : 200인지 아닌지 검증
                 .andExpect(content().string(hello)); // mvc.perform의 결과를 검증 : 응답 본문의 내용(hello)을 검증
-
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception{
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(
+                    get("/hello/dto")
+                        .param("name", name) // param : API 테스트할 때 사용될 요청 파라미터 설정 (단, String만 가능)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+       }
 }

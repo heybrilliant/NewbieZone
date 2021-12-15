ad6a9af05b247dd523374256ee9139807423b251
EunKyung Shin <heybrilliante@gmail.com>
2021-12-15 17:24:47 +0900

    211215 update

--- NewbieProject_SpringBoot/newbie_project/src/main/java/com/newbie/springboot/web/IndexController.java
+++ NewbieProject_SpringBoot/newbie_project/src/main/java/com/newbie/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
-package com.newbie.springboot.web.dto;
+package com.newbie.springboot.web;
 
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 

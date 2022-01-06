05d8ba9442b626d9985ed4939a87ff1581a7b655
EunKyung Shin <heybrilliante@gmail.com>
2022-01-06 15:36:32 +0900

    220106 create domain and repository

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+
+    // repo에 4가지 기능을 만듬
+    // save : 회원이 저장소에 저장이 됨
+    // 그 다음부터 이 저장소에서 findById나 findByName으로 찾아올 수 있음
+    // findAll로 지금까지 저장된 회원의 모든 정보를 List형식으로 받아올 수 있음
+
+
+}
---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,39 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static long sequence = 0L;
+    //sequence : key값을 0,1,2, ... 이렇게 생성해주는 역할
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence); // sequence값을 하나씩 올려줌
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+        // Optional.ofNullable()로 감싸주면 null값이 반환돼도 client측에서 조정 가능
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+                // java lambda 사용 (돌면서 찾음)
+                // member에서 넘어온 값 중에 getName으로 이름이 같은 것을 찾으면 반환
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+}

3753a66a1c647e7369bb1a988b5f005218963009
EunKyung Shin <heybrilliante@gmail.com>
2022-01-07 08:55:38 +0900

    220107 create service

---
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,35 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 회원가입
+    public Long join(Member member){
+        // 같은 이름이 있는 중복 회원은 가입 불가능
+        // 예전에는 if null로 반환을 했었지만 지금은 null일 가능성이 있다면
+        // Optional로 한 번 감싸주고 코딩함
+       /*
+       Optional<Member> result = memberRepository.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+        */
+        memberRepository.findByName(member.getName())
+                        .ifPresent(m -> {
+                            throw new IllegalStateException("이미 존재하는 회원입니다.");
+                        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+
+}
